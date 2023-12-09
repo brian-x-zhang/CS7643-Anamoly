667b10fad05d92cc3390d82471f6d2b970c99f84
Brian Zhang <brianxicheng@gmail.com>
2023-12-09 15:17:30 -0700

    format main.py

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import pandas as pd
 from HyperparameterTuning import tune_clstm, tune_autoencoder
 
-clstm_tuning_results, clstm_model = tune_clstm()
+def main():
+    clstm_tuning_results, clstm_model = tune_clstm()
 
+    writer = pd.ExcelWriter('HyperparameterTuning.xlsx')
+    clstm_tuning_results.to_excel(writer, 'CLSTM')
+    writer.close()
 
-writer = pd.ExcelWriter('HyperparameterTuning.xlsx')
-clstm_tuning_results.to_excel(writer, 'CLSTM')
-writer.close()
+    print('done')
 
-print('done')
+if __name__ == "__main__":
+    main()
