85689359ba625cdbf096e70075ac6e4243158bbf
Brian Zhang <brianxicheng@gmail.com>
2023-12-09 15:13:11 -0700

    print tuning results to excel

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+import pandas as pd
 from HyperparameterTuning import tune_clstm, tune_autoencoder
 
 clstm_tuning_results, clstm_model = tune_clstm()
+
+
+writer = pd.ExcelWriter('HyperparameterTuning.xlsx')
+clstm_tuning_results.to_excel(writer, 'CLSTM')
+writer.close()
 
 print('done')
