a48ff01744ab1d5b46400d527345c19b5818e75f
Brian Zhang <brianxicheng@gmail.com>
2023-12-10 15:15:23 -0700

    add hyperparameter tuning results table

--- latex_generator.py
+++ latex_generator.py
@@ -3,6 +3,6 @@ import pandas as pd
 autoenc = pd.read_csv('autoencoder_tuning_2024-12-10.csv')
 clstm = pd.read_csv('clstm_tuning_2024-12-10.csv')
 
-print(autoenc.to_latex(caption='Autoencoder Hyperparameter Tuning Results', index=False))
-
-print(clstm.to_latex(caption='C-LSTM Hyperparameter Tuning Results', index=False))
+a = autoenc.drop(labels='Unnamed: 0', axis=1).to_latex(caption='Autoencoder Hyperparameter Tuning Results', index=False, escape=True)
+print(a)
+# print(clstm.drop(labels='Unnamed: 0', axis=1).to_latex(caption='C-LSTM Hyperparameter Tuning Results', index=False))
